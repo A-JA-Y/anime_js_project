7a5e5870907390889887bb0ada739668f81cbb42
Ajay <mailtoajay7007@gmail.com>
2024-10-08 23:08:38 +0530

    services page

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import laundry from '../laundry.png'
+import tailor from '../yessir.png'
 
 function Services() {
   return (
-    <div> Services Page</div>
+    <><div className="services bg-black p-5 text-white font-[Poppins] flex flex-col justify-center items-center">
+
+      <h1 className='text-7xl font-extrabold'>Services</h1>
+      <div className="services">
+        <div className="service">
+          <img src={laundry} alt="Laundry" />
+          <h2 className='text-6xl font-thin'>Laundry</h2>
+          <div className="container">
+
+          </div>
+          
+
+              
+            
+         
+          <p>Get your clothes washed, dried, and ironed with our laundry service.</p>
+        </div>
+        <div className="service">
+          <img src={tailor} alt="Tailoring" />
+          <h2 className='text-6xl font-thin'>Tailoring</h2>
+          <div className="container">
+
+          </div>
+          <p>Get your clothes altered and tailored to fit you perfectly with our tailoring service.</p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
